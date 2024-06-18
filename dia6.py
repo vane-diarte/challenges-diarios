e518043a29dfee788e33eb47b3b999482e0e31da
vane-diarte <vanediarte90@gmail.com>
2024-06-18 15:10:35 -0400

    dia6

---
+++ dia6.py
@@ -0,0 +1,3 @@
+temperatura_grados = int(input('Ingese la tempretatura en grados centigrados: '))
+temperatura_fahrenheit = (temperatura_grados * 9/5) + 32
+print(f'la temperatura en fahrenheit es {temperatura_fahrenheit} F°')

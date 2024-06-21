91176f875988269173a22e9cf520d9acb6dfd5c6
vane-diarte <vanediarte90@gmail.com>
2024-06-21 13:35:36 -0400

    dia9

---
+++ dia9.cpp
@@ -0,0 +1,16 @@
+//Escribir un programa que pida al usuario dos números y los sume.
+#include <iostream>
+
+using namespace std;
+
+main(){
+    int a, b, suma;
+    cout << "\n Suma de dos numeros ";
+    cout << "\n ingrese el primer numero: ";
+    cin>>a;
+    cout <<"\n Ingrese el segundo numero: ";
+    cin>>b;
+    suma = a + b;
+    cout<<"\n la suma de "<<a<<"+"<<b<<" es: "<<suma;
+    
+}
